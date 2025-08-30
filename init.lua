ef4fb85e12fed0eafe54954d9cfc0ce156ffca6d
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-08-30 20:43:39 +0000

    Update init.lua for neovim

--- init.lua
+++ init.lua
@@ -1,9 +1,17 @@
 if vim.g.neovide then
-  vim.g.neovide_floating_corner_radius = 1.0
-  vim.g.neovide_refresh_rate = 40
-  vim.g.neovide_no_idle = true
-  vim.g.neovide_cursor_antialiasing = true
-  vim.g.neovide_cursor_vfx_mode = "torpedo"
+	vim.o.guifont = "CaskaydiaCove_NF:h10:b:#e-subpixelantialias:#h-none"
+	vim.g.neovide_scale_factor = 1.0
+	vim.g.neovide_floating_shadow = false
+	vim.g.neovide_floating_z_height = 0
+	vim.g.neovide_light_angle_degrees = 45
+	vim.g.neovide_light_radius = 5
+	vim.g.neovide_floating_corner_radius = 0.0
+	vim.g.neovide_opacity = 1.0
+	vim.g.neovide_normal_opacity = 1.0
+	vim.g.neovide_refresh_rate = 60
+	vim.g.neovide_no_idle = true
+	vim.g.neovide_cursor_antialiasing = false
+	vim.g.neovide_cursor_animate_in_insert_mode = true
 end
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
